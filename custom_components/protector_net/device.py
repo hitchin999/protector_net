4ef46a03f3ab3965f8dc5499119f6b5ae143a42f
hitchin999 <hitchin999@gmail.com>
2025-06-16 20:59:06 -0400

    Update device.py

--- custom_components/protector_net/device.py
+++ custom_components/protector_net/device.py
@@ -1,16 +1,15 @@
 # custom_components/protector_net/device.py
-
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity import Entity
 from .const import DOMAIN
 
 class ProtectorNetDevice(Entity):
-    """Mixin for all Protector.Net entities to share one DeviceInfo."""
-
-    _attr_device_info = DeviceInfo(
-        identifiers={(DOMAIN, "protector_net_main")},
-        name="Protector.Net Access Control",
-        manufacturer="Yoel Goldstein/Vaayer LLC",
-        model="Protector.Net Integration",
-        entry_type="service",
-    )
+    def __init__(self, config_entry):
+        # use the entry_id so each install is its own device
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, config_entry.entry_id)},
+            name=f"Protector.Net @ {config_entry.data['base_url']}",
+            manufacturer="Yoel Goldstein/Vaayer LLC",
+            model="Protector.Net Integration",
+            entry_type="service",
+        )
